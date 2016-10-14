89fae746d3fb0ef3e2f0952b0f5380aef82ae520
General Assembly <generalassembly@Generals-Air-7.home>
2016-10-14 13:04:45 -0400

    Create works.

--- tunrAngular/js/artists/index.controller.js
+++ tunrAngular/js/artists/index.controller.js
@@ -2,6 +2,13 @@ angular.module('artists')
 .controller("ArtistsIndexController", ["ArtistFactory", ArtistsIndexControllerCallback]);
 
 function ArtistsIndexControllerCallback(ArtistFactory){
+  var vm = this
   this.artists = ArtistFactory.query();
+  this.newArtist = new ArtistFactory()
+  this.create = function() {
+    vm.newArtist.$save().then(function(res){
+      vm.artists.push(res);
+    })
+  }
   console.log(this.artists);
 }
